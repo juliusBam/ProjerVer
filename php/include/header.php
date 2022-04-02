760ae35389b29172e4222c0e244b2e8489f8646a
LucasHausi <l.hausensteiner@gmail.com>
2022-04-02 23:34:40 +0200

    Dynamic Header for Index plus basic Navbar

---
+++ php/include/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/myStyle.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <title>ProjerVer</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand mb-0 h1 mt-2 ms-3">ProjerVer</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mt-2">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Board</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Chat</a>
+      </li>
+    </ul>
+  </div>
+</nav>
