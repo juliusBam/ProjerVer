2069054eb84bd116b38b0521ccab910d7a17ade0
Julio A. Bampi <80284467+juliusBam@users.noreply.github.com>
2022-07-03 17:52:53 +0200

    Exclude effects

--- javaScript/scriptIndex.js
+++ javaScript/scriptIndex.js
@@ -99,15 +99,15 @@ $(document).ready(function(){
     //hides the class for the title message first so that it can have an animated appearance
     $(".pt-6").hide();
     $(".pt-6").fadeIn(2000);
-    $(".text").hide();
+    //$(".text").hide();
     //effects when the mouse hovers
-    $(".content").mouseover(function(){
+    /*$(".content").mouseover(function(){
         $(this).children().fadeIn(1000);
     });
     //if the mouse moves out of range the just shown information gets hidden again
     $(".content").mouseout(function(){
         $(this).children().hide();
-    });
+    });*/
     //populates the container with the stats
     getDailyStats("dailyStats");
     getTimeStats("timeStats");
