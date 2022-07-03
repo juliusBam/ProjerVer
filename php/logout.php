0067511db592d59e47b374fd78efdb0571bce8be
ZiraelMon <101460129+ZiraelMon@users.noreply.github.com>
2022-07-03 15:26:01 +0200

    Update Commenting

--- php/logout.php
+++ php/logout.php
@@ -4,10 +4,10 @@
 // session_unset();
 // session_destroy();
 
+// delete cookies, if the user wants to logout
 setcookie('userID', "", time() - 3600);
 setcookie('userName', "", time() - 3600);
 setcookie('userType', "", time() - 3600);
 
-    // ----- TODO -----
-    //how to get out to login.php ??
+// if the user clicks on the submit button, the user will be directed to the index page
 header("location: index.php");
