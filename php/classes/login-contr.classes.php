cd35602fb82b08769ba6d2d39e553f5d685203b3
ZiraelMon <101460129+ZiraelMon@users.noreply.github.com>
2022-07-03 15:13:06 +0200

    Update: Commenting

--- php/classes/login-contr.classes.php
+++ php/classes/login-contr.classes.php
@@ -1,26 +1,34 @@
 <?php
 
+// create a class with properties and methods
 class loginContr extends Login {
 
+    // properties needed for the login
+    // set properties private, that only controllers have access to it
     private $uid;
     private $pwd;
-    
+    // grab data that user that user submitted and assign them to the properties above
     public function __construct($uid, $pwd) {
+        // point to the properties in the class
         $this->uid = $uid;
         $this->pwd = $pwd;
     }
 
+    // if empty input was checked and everything is fine, log in the user
     public function loginUser() {
         if($this->emptyInput() == false) {
             // echo "Empty input!";
 
+            // if an input is empty, run he error message
             header("location: ../login.php?error=emptyinput");
             exit();
         }
 
+        // get user from the database and log in
         $this->getUser($this->uid, $this->pwd);
     }
 
+    // method to check if the input (unsername and password) is empty 
     private function emptyInput() {
         if(empty($this->uid) || empty($this->pwd)) {
             $result = false;
